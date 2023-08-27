6d2648a1025ff075f090937d9f179a81d56ffeb5
Ronel518 <138523963+Ronel518@users.noreply.github.com>
2023-08-27 09:33:44 +0200

    edited spacing for min and max temp

--- src/index.js
+++ src/index.js
@@ -86,10 +86,10 @@ function displayForecast(response) {
         />
  
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max"> :${Math.round(
+        <span class="weather-forecast-temperature-max">:${Math.round(
           forecastDay.temp.max
         )}°C </span>|
-        <span class="weather-forecast-temperature-min"> :${Math.round(
+        <span class="weather-forecast-temperature-min">:${Math.round(
           forecastDay.temp.min
         )}°C </span>
     </div>
